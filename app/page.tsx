a73418288c140bc83c98439555a7722797444d1e
jrmyok <jrmyok@protonmail.com>
2023-06-25 16:04:04 -0700

    change disc

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
               <div className="absolute inset-0 bg-gray-900/90 mix-blend-multiply" />
               <div className="absolute -left-80 -top-56 transform-gpu blur-3xl" aria-hidden="true">
                 <div
-                  className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-cyan-50 to-cyan-700 opacity-[0.45]"
+                  className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-rose-50 to-rose-700 opacity-[0.45]"
                   style={{
                     clipPath:
                       'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -129,7 +129,7 @@ export default function Home() {
                 aria-hidden="true"
               >
                 <div
-                  className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-teal-300 to-teal-800 opacity-25"
+                  className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-rose-300 to-red-800 opacity-25"
                   style={{
                     clipPath:
                       'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
