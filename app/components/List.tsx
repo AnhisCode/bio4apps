ef67bd4747174858f219c1299e0ae3bb366a7743
jrmyok <jrmyok@protonmail.com>
2023-06-25 14:41:08 -0700

    fix mobile list

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -7,14 +7,14 @@ export default function List({ topics }: { topics: { title: string; list: string
   const [selected, setSelected] = useState(topics[0]);
 
   return (
-    <div className="flex w-full space-x-3 flex-col md:flex-row  ">
-      <div className={'flex relative items-center justify-between w-1/2 flex-shrink-0'}>
+    <div className="flex w-full space-x-3 flex-col lg:flex-row  ">
+      <div className={'flex relative items-center justify-between w-full lg:w-1/2 flex-shrink-0'}>
         <ul className="text-left text-white flex flex-col space-y-6 my-10 ">
           {topics.map((topic, index) => (
             <li
               key={`title-${index}-${topic.title}`}
               className={classNames(
-                'text-2xl lg:text-3xl font-infant hover:text-red-600 hover:cursor-pointer transition-colors duration-200 ease-in-out',
+                'text-3xl lg:text-3xl font-infant hover:text-red-600 hover:cursor-pointer transition-colors duration-200 ease-in-out',
                 topic.title === selected.title && 'text-gray-400',
               )}
               onClick={() => setSelected(topic)}
@@ -23,11 +23,14 @@ export default function List({ topics }: { topics: { title: string; list: string
             </li>
           ))}
         </ul>
-        <div className={'w-[1px] hidden md:flex h-full bg-gray-300 right-10 lg:right-40 sm:right-20 absolute'}></div>
+        <div className={'w-[1px] hidden lg:flex h-full bg-gray-300 right-10 lg:right-40 sm:right-20 absolute'}></div>
       </div>
-      <ul className="flex space-y-3 justify-center flex-col w-full text-left text-white list-disc list-inside marker:text-gray-500 marker:text-4xl ">
+      <ul className="flex space-y-3 justify-center flex-col w-full text-left text-white list-disc list-inside marker:text-red-800/50 marker:text-4xl ">
         {selected.list.map((item, index) => (
-          <li key={`${index}-${item}`} className="text-xl font-sans whitespace-nowrap normal-case font-light">
+          <li
+            key={`${index}-${item}`}
+            className="lg:text-xl text-lg font-sans whitespace-nowrap normal-case font-light"
+          >
             {item}
           </li>
         ))}
