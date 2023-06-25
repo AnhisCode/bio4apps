1cffb72c33da3fad8b7fde0a984197401482af26
jrmyok <jrmyok@protonmail.com>
2023-06-25 16:02:12 -0700

    add image location

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
             >
               SUBMIT NOW
             </button>
-            <span className={'font-jura'}>(Due by 15 November 2023)</span>
+            <span className={'font-jura text-gray-500'}>(Due by 15 November 2023)</span>
           </section>
         </div>
       </div>
@@ -103,11 +103,47 @@ export default function Home() {
         </div>
 
         <div className="w-1/2 h-[1px] bg-black mt-4"></div>
-        <div>
-          <span className={'font-jura text-xl'}>Venue</span>
-          <h1 className={`text-xl font-medium text-gray-900 sm:text-2xl normal-case`}>
-            Griffith University, Gold Coast Campus. 1 Parklands Drive, Southport, QLD 4215.
-          </h1>
+
+        <div className="bg-white ">
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="relative overflow-hidden bg-gray-900 px-6 py-20 shadow-xl sm:rounded-3xl sm:px-10 sm:py-24 md:px-12 lg:px-20">
+              <Image
+                className="absolute inset-0 h-full w-full object-cover brightness-150 saturate-0"
+                src="/location.jpeg"
+                alt=""
+                width={1000}
+                height={1000}
+              />
+              <div className="absolute inset-0 bg-gray-900/90 mix-blend-multiply" />
+              <div className="absolute -left-80 -top-56 transform-gpu blur-3xl" aria-hidden="true">
+                <div
+                  className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-cyan-50 to-cyan-700 opacity-[0.45]"
+                  style={{
+                    clipPath:
+                      'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                  }}
+                />
+              </div>
+              <div
+                className="hidden md:absolute md:bottom-16 md:left-[50rem] md:block md:transform-gpu md:blur-3xl"
+                aria-hidden="true"
+              >
+                <div
+                  className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-teal-300 to-teal-800 opacity-25"
+                  style={{
+                    clipPath:
+                      'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                  }}
+                />
+              </div>
+              <div className="relative mx-auto max-w-2xl lg:mx-0">
+                <span className={'font-jura text-xl text-white'}>Venue</span>
+                <h1 className={`text-xl font-medium text-white sm:text-2xl normal-case`}>
+                  Griffith University, Gold Coast Campus. 1 Parklands Drive, Southport, QLD 4215.
+                </h1>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <div className="relative min-h-screen bg-gray-950">
