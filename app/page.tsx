29cc59b675c78b197ada45016ab23e691c401fb0
jrmyok <jrmyok@protonmail.com>
2023-06-25 11:44:28 -0700

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -75,26 +75,28 @@ const Person = ({ name, location }: { name: string; location: string }) => (
 export default function Home() {
   return (
     <main className="bg-white">
-      <div className={'bg-hero-pattern bg-cover bg-no-repeat'}>
-        <section className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 text-center min-h-screen flex flex-col gap-y-4 items-center justify-center h-full font-infant uppercase">
-          <h3 className="text-3xl font-jura font-extrabold text-gray-900 sm:text-4xl">2023</h3>
-          <h1 className={`text-6xl font-extrabold text-gray-900 sm:text-8xl`}>BIO4APPS</h1>
-          <p className="font-jura mt-4 text-lg text-gray-500">
-            Bio4Apps 2023 is an international conference for researchers, scientists, and engineers throughout the world
-            to present their latest findings, ideas, and applications in the areas of biosensors, bioelectronics,
-            biomedical devices, and bioMES /NEMS.
-          </p>
-          <button
-            className={`font-sans mt-8 w-48 px-8 py-3 border border-transparent text-base font-medium rounded-full text-white bg-black hover:bg-gray-800 
+      <div style={{}} className={'bg-hero-pattern bg-cover bg-no-repeat bg-slate-50 '}>
+        <div className={'backdrop-blur-sm'}>
+          <section className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 text-center min-h-screen flex flex-col gap-y-4 items-center justify-center h-full font-infant uppercase ">
+            <h3 className="text-3xl font-jura font-extrabold text-gray-900 ">2023</h3>
+            <h1 className={`text-6xl font-extrabold text-gray-900 sm:text-8xl`}>BIO4APPS</h1>
+            <p className="font-jura mt-4 text-lg text-black">
+              Bio4Apps 2023 is an international conference for researchers, scientists, and engineers throughout the
+              world to present their latest findings, ideas, and applications in the areas of biosensors,
+              bioelectronics, biomedical devices, and bioMES /NEMS.
+            </p>
+            <button
+              className={`font-sans mt-8 w-48 px-8 py-3 border border-transparent text-base font-medium rounded-full text-white bg-black hover:bg-gray-800 
           `}
-          >
-            SUBMIT NOW
-          </button>
-          <span className={'font-jura'}>(Due by 15 November 2023)</span>
-        </section>
+            >
+              SUBMIT NOW
+            </button>
+            <span className={'font-jura'}>(Due by 15 November 2023)</span>
+          </section>
+        </div>
       </div>
 
-      <section className="pb-56  py-12 mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 text-center  flex flex-col gap-y-24 items-center justify-center h-full font-infant uppercase">
+      <section className="min-h-screen mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 text-center  flex flex-col gap-y-24 items-center justify-center h-full font-infant uppercase">
         <div>
           <span className={'font-jura text-xl'}>Date of conference</span>
           <h1 className={`text-4xl font-extrabold text-gray-900 sm:text-6xl`}>15-17 November 2023</h1>
@@ -108,7 +110,31 @@ export default function Home() {
           </h1>
         </div>
       </section>
-      <div className=" min-h-screen bg-gray-950">
+      <div className="relative min-h-screen bg-gray-950">
+        <svg
+          className="absolute inset-0 z-0 h-full w-full stroke-gray-50/50 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
+              width={200}
+              height={200}
+              x="50%"
+              y={-64}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-64} className="overflow-visible fill-red-900/20">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M299.5 800h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
+        </svg>
         <section className="overflow-hidden relative min-h-screen mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center flex flex-col gap-y-4 items-start justify-center h-full font-infant uppercase">
           <h1 className={`absolute top-20 text-lg  font-jura text-gray-200`}>Topics this year</h1>
           <List topics={data} />
